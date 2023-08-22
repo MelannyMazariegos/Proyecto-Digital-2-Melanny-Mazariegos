6f0f66dd7b81402a2a9ec7d811bb9d5f2d77c838
MelannyMazariegos <maz21055@uvg.edu.gt>
2023-08-21 18:14:01 -0600

    Update main.cpp

--- Proyecto-Digital2-Mazariegos/src/main.cpp
+++ Proyecto-Digital2-Mazariegos/src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#define rojo 23
+#define amarillo 21
+#define verde 22
+#define botemp 19
+#define sensortemp 35
 
-// put function declarations here:
-int myFunction(int, int);
+const int debounceDelay = 50;
+unsigned long lastDebounceTime1 = 0;
+bool lastButtonState1 = HIGH;
+bool buttonState1 = HIGH;
+int ADCraw = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(rojo, OUTPUT);
+  pinMode(amarillo, OUTPUT);
+  pinMode(verde, OUTPUT);
+  pinMode(botemp, INPUT_PULLUP);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int reading1 = digitalRead(botemp);
+  if ((millis() - lastDebounceTime1) > debounceDelay) {
+    if (reading1 != lastButtonState1) {
+      lastDebounceTime1 = millis();
+      lastButtonState1 = reading1;
+      if (lastButtonState1 == LOW) {
+        ADCraw = analogRead(sensortemp);
+      }
+    }
+  }
 }
