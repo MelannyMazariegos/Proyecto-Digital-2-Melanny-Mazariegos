68b33689826c56ea76342b653af2c084a46f3f0d
MelannyMazariegos <maz21055@uvg.edu.gt>
2023-08-26 22:21:23 -0600

    sensor de temperatura y comunicacion serial

--- Proyecto-Digital2-Mazariegos/src/main.cpp
+++ Proyecto-Digital2-Mazariegos/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <driver/ledc.h>
+#include <esp_adc_cal.h>
 #define rojo 23
 #define amarillo 21
 #define verde 22
@@ -10,6 +12,15 @@ unsigned long lastDebounceTime1 = 0;
 bool lastButtonState1 = HIGH;
 bool buttonState1 = HIGH;
 int ADCraw = 0;
+
+float temperatura = 0.0;
+
+
+uint32_t readADC_Cal(int ADC_RAW){
+  esp_adc_cal_characteristics_t adc_chars;
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+  return (esp_adc_cal_raw_to_voltage(ADC_RAW, &adc_chars));
+}
 
 void setup() {
   pinMode(rojo, OUTPUT);
@@ -27,6 +38,8 @@ void loop() {
       lastButtonState1 = reading1;
       if (lastButtonState1 == LOW) {
         ADCraw = analogRead(sensortemp);
+        float voltaje = readADC_Cal(ADCraw);
+        temperatura = ((voltaje/4095)*3.3)/0.01;
       }
     }
   }
